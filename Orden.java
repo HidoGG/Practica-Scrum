767651e957386babb5e34883628dec08e3b2ceeb
MatiasNicolas2013 <112669826+MatiasNicolas2013@users.noreply.github.com>
2023-04-11 00:20:15 +0200

    Class Orden-Codigo
    Subo el codigo de ClassOrden

---
+++ Orden.java
@@ -0,0 +1,28 @@
+
+public class Orden {
+private final int idOrden;
+private Computadora computadora [];
+private static int contadorOrdenes = 0;
+private static final int MAX_COMPUTADORAS = 10;
+private int contadorComputadora;
+   
+public Orden() {
+    this.idOrden = ++Orden.contadorOrdenes;
+    this.computadora = new Computadora[Orden.MAX_COMPUTADORAS];
+    this.contadorComputadora = 0;
+}      
+            
+
+
+
+
+
+public static void agregarComputadora(Computadora computadora){ 
+
+        if (this.contadorComputadora < Orden.MAX_COMPUTADORAS)
+        {this.computadora[this.contadorComputadora++] = computadora;
+                
+    }
+    
+}
+}
